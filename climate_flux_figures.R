8c4facb76684cd8b19faa112cef19b740474562a
Elizabeth <duane@uci.edu>
2023-04-27 15:59:49 -0700

    figure updates

--- climate_flux_figures.R
+++ climate_flux_figures.R
@@ -11,10 +11,7 @@ library(cowplot)
 library(ggpubr)
 
 ########## Load in data ##########
-wthr_FMC <- read_csv("weather_flux/data/processed/weather_stations/wthr_1hr_FMC.csv") %>%
-  filter(site!="HQ_AWC")
-SILO <- read_csv("weather_flux/data/external_data/SILO_processed.csv") %>%
-  filter(site!="HQ_AWC")
+wthr_FMC <- read_csv("weather_flux/data/processed/weather_stations/wthr_1hr_FMC.csv")
 pine_flux <- read_csv("weather_flux/data/processed/wood_respiration/pine_CO2_clean.csv") %>%
   filter(CO2_resp_rate > 0)
 native_flux <- read_csv("weather_flux/data/processed/wood_respiration/native_CO2_clean.csv")
@@ -445,21 +442,6 @@ ggplot() +
 dev.off()
 
 #..Weather data ####
-#......Weather for each site (rainfall and temperature) ####
-png("figures/S_temp_rain.png",width=2000,height=2000,res=250)
-ggplot(wthr_FMC) + 
-  #geom_point(aes(date,AirTC_Avg),color="red",alpha=0.2,size=0.25) +
-  geom_line(aes(date,AirTC_Avg),stat="identity",color="red",alpha=0.5) + 
-  geom_bar(aes(date,Rain_mm_Tot),stat="identity",color="blue") + 
-  facet_grid2(fct_relevel(site,"DRO","MLRF","MLES","STCK","PNW")~.) +
-  xlab(label = "Date") + 
-  scale_y_continuous(name="Air Temperature (C)",
-                     sec.axis=sec_axis(~.,name="Rainfall (mm/hr)")) +
-  fig_aes
-dev.off()
-
-
-
 #......Gap-filled weather data ####
 pdf(file="figures/S_wthr_gap_filled.pdf",width=11,height=8.5)
 ggplot(wthr_FMC,aes(date,AirTC_Avg,color=AirTC_source)) + 
@@ -526,81 +508,6 @@ ggplot(wthr_FMC,aes(date,FMC_norm)) +
   facet_grid(fct_relevel(site,"DRO","MLRF","MLES","STCK","PNW")~.) +
   fig_aes
 dev.off()
-
-
-
-#......Table of annual rainfall and precipiation across experiment ####
-wthr_rain_avg <- wthr_FMC %>%
-  mutate(year = year(date),
-         month = month(date),
-         Rain_mm_f = ifelse(is.na(Rain_mm),PRECTOTCORR,Rain_mm)) %>%
-  mutate(year_half = case_when(month%in%c(6:12) ~ "second",
-                            TRUE ~ "first")) %>%
-  group_by(site, year, year_half) %>%
-  summarize(Rain_tot = sum(Rain_mm_Tot),
-            Rain_tot_CCS = sum(Rain_mm_f)) %>%
-  ungroup() %>%
-  mutate(Rain_tot_yr = Rain_tot + dplyr::lag(Rain_tot),
-         Rain_tot_yr_CCS = Rain_tot_CCS + dplyr::lag(Rain_tot_CCS)) %>%
-  filter(year_half=="first") %>%
-  group_by(site) %>%
-  summarize(Rain_avg = mean(Rain_tot_yr),
-            Rain_avg_CCS = mean(Rain_tot_yr_CCS))
-
-wthr_TC_avg <- wthr_FMC %>%
-  mutate(year = year(date)) %>%
-  group_by(site,year) %>%
-  summarize(AirTC_mean = mean(AirTC_Avg),
-            AirTC_max = max(AirTC_Avg),
-            AirTC_min = min(AirTC_Avg),
-            ibTC_mean = mean(ib_AirTC_Avg),
-            ibTC_max = max(ib_AirTC_Avg),
-            ibTC_min = min(ib_AirTC_Avg))
-
-
-
-#......Rainfall gap-filling comparison with SILO data ####
-SILO_rain_f <- SILO %>%
-  filter(day > as.POSIXlt("2018-06-04")) %>%
-  filter(day < as.POSIXlt("2022-06-14")) %>%
-  mutate(year_mon = as.yearmon(day)) %>%
-  group_by(site,year_mon) %>%
-  summarize(SILO = sum(daily_rain, na.rm = TRUE))
-
-wthr_rain_val <- wthr_FMC %>%
-  mutate(Rain_CP = ifelse(is.na(Rain_mm),PRECTOTCORR,Rain_mm)) %>%
-  mutate(day = date(date),
-         year_mon = as.yearmon(day)) %>%
-  group_by(site,year_mon) %>%
-  summarize(CHRS_filled = sum(Rain_mm_Tot),
-            POWER_filled = sum(Rain_mm_Tot_P),
-            CP_only = sum(Rain_CP))
-wthr_rain <- wthr_FMC %>%
-  mutate(day = date(date),
-         year_mon = as.yearmon(day)) %>%
-  group_by(site,year_mon) %>%
-  count(Rain_source) %>%
-  top_n(1) %>%
-  select(-n) %>%
-  left_join(wthr_rain_val,by=c("site","year_mon"))
-
-png("figures/S_wthr_rain_comparison.png",width=2000,height=2000,res=250)
-ggplot() + 
-  geom_point(data=SILO_rain_f,aes(year_mon,SILO),color=p_SILO) + 
-  geom_line(data=SILO_rain_f,aes(year_mon,SILO),color=p_SILO,linetype="dashed") + 
-  geom_point(data=wthr_rain,aes(year_mon,CHRS_filled,color=Rain_source)) +
-  geom_line(data=wthr_rain,aes(year_mon,CHRS_filled),color=p_WTF) + 
-  scale_color_manual(name="Rainfall source",values =c(p_CHRS,p_WTF),
-                     labels=c("CHRS CCS", "WTF Stations")) + 
-  facet_grid(fct_relevel(site,"DRO","MLRF","MLES","STCK","PNW")~.) + 
-  xlab("Month") + ylab("Rainfall (mm/month)") +
-  fig_aes
-dev.off()
-
-
-
-
-
 
 
 
